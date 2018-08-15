59700daa3a65248321691761cfd59daea016a093
himanshu0503 <hemanshu0503@gmail.com>
2018-08-15 18:13:32 +0530

    Adds middleware

--- api/middleware/validateUser.js
+++ api/middleware/validateUser.js
@@ -4,26 +4,23 @@ const users = require('../../models/users');
 const _ = require('underscore');
 
 module.exports = (req, res, next) => {
-  var bag = {
-    req: req,
-    reqQuery: req.query || {},
-    inputParams: req.params || {},
-    reqPath: req.route.path,
-  };
-  req.shim = req.shim || {};
+  req.shim = {};
 
-  bag.who = util.format('%s|%s|%s', req.method, req.url, 'validateUser');
-  logger.debug(bag.who, 'Started');
+  //LOGIC to parse basicAuth
+  var auth = req.headers['authorization'];
+  var tmp = auth.split(' ');
+  var buf = new Buffer(tmp[1], 'base64');
+  var plain_auth = buf.toString();
+  var creds = plain_auth.split(':');
+  var username = creds[0];
+  var password = creds[1];
 
-  if (!req.cookies.authToken && !req.headers.authorization)
-    return res.send('Unauthorized').status(401);
-
-  let authToken = req.headers.authorization || req.cookies.authToken;
   let query = {
     where: {
-      authToken: authToken
+      email: username,
+      password: password
     }
-  }
+  };
   //TODO: Replace this with redis
   users.findOne(query).asCallback(
     function (err, user) {
--- api/router.js
+++ api/router.js
@@ -2,10 +2,10 @@
 function route (app) {
   let validateUser = require('./middleware/validateUser');
 
-  app.post('/api/v1/contacts', require('./contacts/post'));
-  app.put('/api/v1/contacts/:contactId', require('./contacts/put'));
-  app.delete('/api/v1/contacts/:contactId', require('./contacts/delete'));
-  app.get('/api/v1/contacts/search/', require('./contacts/getS'));
+  app.post('/api/v1/contacts', validateUser, require('./contacts/post'));
+  app.put('/api/v1/contacts/:contactId', validateUser, require('./contacts/put'));
+  app.delete('/api/v1/contacts/:contactId', validateUser, require('./contacts/delete'));
+  app.get('/api/v1/contacts/search/', validateUser, require('./contacts/getS'));
 }
 
 module.exports.route = route;
